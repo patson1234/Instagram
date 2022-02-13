fcd21367c324cb28962999d20a665284bb00dbe8
patson1234 <89961562+patson1234@users.noreply.github.com>
2022-02-13 11:45:34 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import { getAuth} from "firebase/auth";
 import {getFirestore} from 'firebase/firestore'
 import {getStorage} from 'firebase/storage'
 const firebaseConfig = {
-  apiKey: "AIzaSyD2tRJOnmSzgSmFR5OyG_jLgcEWRNe96cg",
-  authDomain: "test-df5ac.firebaseapp.com",
-  projectId: "test-df5ac",
-  storageBucket: "test-df5ac.appspot.com",
-  messagingSenderId: "495611272405",
-  appId: "1:495611272405:web:7162a02ee72752eb8e965c",
-  measurementId: "G-8K9VMXQ5WB"
+  //FIREBASE CONFIG
 };
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
 const db = getFirestore()
